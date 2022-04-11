58001e9dd2c5bd96d65d1acd9929b2eb6a2b7237
angusingram <angus214@ad.unc.edu>
2022-04-11 12:14:11 -0400

    final bug fixes i hope

--- flips.js
+++ flips.js
@@ -2,7 +2,15 @@
 import { coinFlips, countFlips } from "./modules/coin.mjs";
 
 const args = process.argv.slice(2);
-args[0] = args[0].slice(9);
-let array1 = coinFlips(args[0]);
-process.stdout.write(array1);
-process.stdout.write(countFlips(array1));
+if (args[0] == null) {
+    let array1 = coinFlips(args[0]);
+    console.log(array1);
+    console.log(countFlips(array1));
+} else {
+    args[0] = args[0].slice(9);
+    let array1 = coinFlips(args[0]);
+    console.log(array1);
+    console.log(countFlips(array1));
+}
+//process.stdout.write(array1);
+//process.stdout.write(countFlips(array1));
--- guess-flip.js
+++ guess-flip.js
@@ -2,5 +2,10 @@
 import { flipACoin } from "./modules/coin.mjs";
 
 const args = process.argv.slice(2);
-args[0] = args[0].slice(7);
-process.stdout.write(flipACoin(args[0]));
+if (args[0] == null) {
+    process.stdout.write(flipACoin(args[0]));
+} else {
+    args[0] = args[0].slice(7);
+    process.stdout.write(flipACoin(args[0]));  
+}
+
