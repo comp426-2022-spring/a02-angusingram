a559505a0bb182db835676a13809c80509639be6
angusingram <angus214@ad.unc.edu>
2022-04-23 15:19:58 -0400

    fixed bad regex

--- modules/coin.mjs
+++ modules/coin.mjs
@@ -25,9 +25,9 @@ export function coinFlip() {
   let x = Math.floor(Math.random() * (max - min + 1) + min);
   x = x % 2;
   if(x == 1) {
-    return "heads";
+    return '"heads"';
   } else if (x == 0) {
-    return "tails";
+    return '"tails"';
   }
 }
 
